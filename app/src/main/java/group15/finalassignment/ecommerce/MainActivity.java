5307d6cfe5f03f509432da606e1d2de058e9bf12
DuyLe2000 <56990897+DuyLe2000@users.noreply.github.com>
2021-12-16 13:36:28 +0700

    Test commit via Android Studio

--- app/src/main/java/group15/finalassignment/ecommerce/MainActivity.java
+++ app/src/main/java/group15/finalassignment/ecommerce/MainActivity.java
@@ -9,34 +9,36 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
 
-        setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_main);
 
 
-        Button btnMainLogin = (Button) findViewById(R.id.main_login_btn);
-        btnMainLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent moveToUserLogin = new Intent(MainActivity.this,UserLoginActivity.class);
-                startActivity(moveToUserLogin);
-                finish();
-            }
-        });
+    Button btnMainLogin = (Button) findViewById(R.id.main_login_btn);
+    btnMainLogin.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent moveToUserLogin = new Intent(MainActivity.this,UserLoginActivity.class);
+        startActivity(moveToUserLogin);
+        finish();
+      }
+    });
 
-        Button btnMainRegister = (Button) findViewById(R.id.main_join_now_btn);
-        btnMainRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent moveToUserRegister = new Intent(MainActivity.this,UserRegisterActivity.class);
-                startActivity(moveToUserRegister);
-                finish();
-            }
-        });
+    Button btnMainRegister = (Button) findViewById(R.id.main_join_now_btn);
+    btnMainRegister.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent moveToUserRegister = new Intent(MainActivity.this,UserRegisterActivity.class);
+        startActivity(moveToUserRegister);
+        finish();
+      }
+    });
 
 
-    }
+  }
 }
+
+// Duy was here Muahahahahaa
