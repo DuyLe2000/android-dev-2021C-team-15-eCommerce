e66085a15ba6bbc5fc1e86a188df68a6f4454c6d
DuyLe2000 <56990897+DuyLe2000@users.noreply.github.com>
2021-12-16 13:28:54 +0700

    Initial commit

---
+++ app/src/main/java/group15/finalassignment/ecommerce/MainActivity.java
@@ -0,0 +1,42 @@
+package group15.finalassignment.ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+
+
+        Button btnMainLogin = (Button) findViewById(R.id.main_login_btn);
+        btnMainLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent moveToUserLogin = new Intent(MainActivity.this,UserLoginActivity.class);
+                startActivity(moveToUserLogin);
+                finish();
+            }
+        });
+
+        Button btnMainRegister = (Button) findViewById(R.id.main_join_now_btn);
+        btnMainRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent moveToUserRegister = new Intent(MainActivity.this,UserRegisterActivity.class);
+                startActivity(moveToUserRegister);
+                finish();
+            }
+        });
+
+
+    }
+}
